00ab5d952e464ea8c371a24374c6eb5112aa287c
Jakob <jakob.g.maier@gmail.com>
2023-10-30 19:45:54 +0100

    add gfx namespace

--- image.h
+++ image.h
@@ -4,32 +4,35 @@
 #include <string>
 #include <glm/glm.hpp>
 
-class Image
+namespace gfx
 {
-public:
-  enum Format : GLint
+  class Image
   {
-    RED = GL_RED,
-    RG = GL_RG,
-    RGB = GL_RGB,
-    RGBA = GL_RGBA
+  public:
+    enum Format : GLint
+    {
+      RED = GL_RED,
+      RG = GL_RG,
+      RGB = GL_RGB,
+      RGBA = GL_RGBA
+    };
+
+    Image(const std::string &path, bool flip_vertically = false);
+    ~Image();
+
+    Image(const Image &other) = delete;
+    Image &operator=(const Image &other) = delete;
+    Image(Image &&other) noexcept;
+    Image &operator=(Image &&other) noexcept;
+
+    unsigned char *data() const;
+    int width() const;
+    int height() const;
+    int channels() const;
+    Format format() const;
+
+  private:
+    unsigned char *m_data = nullptr;
+    int m_width, m_height, m_channels;
   };
-
-  Image(const std::string &path, bool flip_vertically = false);
-  ~Image();
-
-  Image(const Image &other) = delete;
-  Image &operator=(const Image &other) = delete;
-  Image(Image &&other) noexcept;
-  Image &operator=(Image &&other) noexcept;
-
-  unsigned char *data() const;
-  int width() const;
-  int height() const;
-  int channels() const;
-  Format format() const;
-
-private:
-  unsigned char *m_data = nullptr;
-  int m_width, m_height, m_channels;
-};
+}
