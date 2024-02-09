b6017e25e8a5b532bcc3ac31df0a5ffbd8f4ef89
Jakob <jakob.g.maier@gmail.com>
2024-02-09 13:53:52 +0100

    Update to camera

--- camera.cpp
+++ camera.cpp
@@ -8,17 +8,29 @@ glm::mat4 Camera::projection_matrix() const { return m_projection; }
 
 glm::mat4 Camera::view_projection_matrix() const { return projection_matrix() * view_matrix(); }
 
-void Camera::set_projection_matrix(const glm::mat4 &matrix) { m_projection = matrix; }
-
 float Camera::far() const { return m_far; }
 
 float Camera::near() const { return m_near; }
 
-void Camera::set_projection_matrix(float fov, float aspect_ratio, float near, float far)
+float Camera::aspect_ratio() const { return m_aspect_ratio; }
+
+float Camera::fov() const { return m_fov; }
+
+void Camera::set_attributes(float fov, float aspect_ratio, float near, float far)
 {
+  m_fov = fov;
+  m_aspect_ratio = aspect_ratio;
   m_near = near;
   m_far = far;
-  m_projection = glm::perspective(fov, aspect_ratio, near, far);
+  compute_projection_matrix();
 }
+
+void Camera::set_aspect_ratio(float aspect_ratio)
+{
+  m_aspect_ratio = aspect_ratio;
+  compute_projection_matrix();
+}
+
+void Camera::compute_projection_matrix() { m_projection = glm::perspective(m_fov, m_aspect_ratio, m_near, m_far); }
 
 }  // namespace gfx
--- camera.h
+++ camera.h
@@ -8,16 +8,27 @@ class Camera : public Transform
 {
  protected:
   glm::mat4 m_projection{};
-  float m_near{}, m_far{};
+  float m_near, m_far, m_fov, m_aspect_ratio;
+
+  void compute_projection_matrix();
 
  public:
-  Camera() : Transform() {}
+  Camera() : Camera(glm::radians(45.0f), 16.0f / 9.0f, 1.0f, 1000.0f) {}
+
+  Camera(float fov, float aspect_ratio, float near, float far)
+      : Transform(), m_fov(fov), m_aspect_ratio(aspect_ratio), m_near(near), m_far(far)
+  {
+    compute_projection_matrix();
+  }
+
   glm::mat4 view_matrix() const;
   glm::mat4 projection_matrix() const;
   glm::mat4 view_projection_matrix() const;
-  void set_projection_matrix(const glm::mat4& matrix);
-  void set_projection_matrix(float fov, float aspect_ratio, float near, float far);
+  void set_attributes(float fov, float aspect_ratio, float near, float far);
+  void set_aspect_ratio(float aspect_ratio);
   float near() const;
   float far() const;
+  float aspect_ratio() const;
+  float fov() const;
 };
 }  // namespace gfx
